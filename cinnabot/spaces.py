8365c2e7d73c0dca48addf8f0a3fbba4dcfec228
mitchellkwong <mitchellkwong@gmail.com>
2021-01-30 21:49:27 +0800

    Added outline for spaces function

--- cinnabot/spaces.py
+++ cinnabot/spaces.py
@@ -1,6 +1,118 @@
+from datetime import datetime, timedelta
+import pytz
+
+from google.cloud.firestore import Client
 from telegram import Update
 from telegram.ext import CallbackContext
 
-def spaces(update: Update, context: CallbackContext):
-    """Handler for /spaces"""
-    pass
+from .utils import Command
+
+class Spaces(Command):
+    command = 'spaces'
+    help_text = 'list of space bookings'
+    help_full = (
+        "To use the '/spaces' command, type one of the following:\n"
+        "'/spaces' : to view all bookings for today\n"
+        "'/spaces now' : to view bookings active at this very moment\n"
+        "'/spaces week' : to view all bookings for this week\n"
+        "'/spaces dd/mm(/yy)' : to view all bookings on a specific day\n"
+        "'/spaces dd/mm(/yy) dd/mm(/yy)' : to view all bookings in a specific range of dates"
+    )
+
+    def __init__(self, database: Client):
+        self.db = database
+
+    def callback(self, update: Update, context: CallbackContext):
+        """Delegates behaviour to sub-handlers depending on input format"""
+        # /spaces
+        if not context.args:
+            self._spaces(update, context)
+        
+        # /spaces now
+        elif context.args[0].lower() == 'now':
+            self._spaces_now(update, context)
+        
+        # /spaces week
+        elif context.args[0].lower() == 'week':
+            self._spaces_week(update, context)
+        
+        # /spaces dd/mm(/yy)
+        elif len(context.args) == 1:
+            self._spaces_day(update, context)
+        
+        # /spaces dd/mm(/yy) dd/mm(/yy)
+        elif len(context.args) == 2:
+            self._spaces_date_range(update, context)
+
+    def _spaces(self, update: Update, context: CallbackContext):
+        """/spaces"""
+        now = datetime.now()
+        today = datetime(now.year, now.month, now.day) # reset time to midnight
+        tomorrow = today + timedelta(days=1)
+        events = spaces._events_between(today, tomorrow)
+        pass
+
+    def _spaces_now(self, update: Update, context: CallbackContext):
+        """/spaces now"""
+        now = datetime.now()
+        events = spaces._events_between(now, now)
+        pass
+
+    def _spaces_week(self, update: Update, context: CallbackContext):
+        """/spaces week"""
+        pass
+
+    def _spaces_day(self, update: Update, context: CallbackContext):
+        """/spaces dd/mm(/yy)"""
+        pass
+
+    def _spaces_date_range(self, update: Update, context: CallbackContext):
+        """/spaces dd/mm(/yy) dd/mm(/yy)"""
+        pass
+
+    def _events_between(self, start_time: datetime, end_time: datetime):
+        """gets a set of events (E) overlapping with some interval [start_time, end_time].
+        
+        E = {event : event.startDate <= end_time & event.endDate >= start_time}
+        
+        Sadly, firebase doesn't let us perform two inequality queries on different fields
+        simultaneously, so we perform the less intensive query locally.
+        """
+        # Convert to datatype that plays well with firestore's timestamp
+        start_time = pytz.UTC.localize(start_time)
+        end_time = pytz.UTC.localize(end_time)
+
+        # Query: event.endDate >= start_time
+        not_ended = self.db.collection('events') \
+            .where('endDate', '>=', start_time) \
+            .get()
+
+        # Query: event.startDate <= end_time
+        events = [event.to_dict() for event in not_ended]
+        events = [event for event in events if event.get('startDate') <= end_time]
+        
+        return events
+
+if __name__ == "__main__":
+    from datetime import datetime, timedelta
+    from google.cloud.firestore import Client
+
+    # Initialize a backend with a firestore client
+    firestore = Client(project='usc-website-206715')
+    spaces = Spaces(database=firestore)
+    
+    # Test event querying (Actually this block can go in Spaces._spaces)
+    now = datetime.now()
+    today = datetime(now.year, now.month, now.day) # reset time to midnight
+    tomorrow = today + timedelta(days=1)
+
+    events = spaces._events_between(today, tomorrow)
+
+    for event in events:
+        # Each "event" is a dictionary following the schema of documents in firestore
+        print(80*'=')
+        print(event['name'])
+        print(80*'-')
+        print('- Venue:', event['venueName'])
+        print('- start:', event['startDate'])
+        print('- end  :', event['endDate'])
